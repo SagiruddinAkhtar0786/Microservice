0d5843b6e99150772cca852e98cc0aa51369a3dd
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-03-25 01:20:42 +0530

    commit

---
+++ QuizApp4/src/main/java/com/pracatice/QuestionController.java
@@ -0,0 +1,17 @@
+package com.pracatice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+	
+	
+	@GetMapping("allQuestion")
+	public String getAllQuestion() {
+		return "hi";
+	}
+
+}
