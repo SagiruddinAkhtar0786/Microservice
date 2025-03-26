0a5c35157415e2788aefb891f0cc514d2659d06d
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-03-26 17:20:24 +0530

    commit

--- QuizApp4/src/main/java/com/pracatice/Controller/QuizController.java
+++ QuizApp4/src/main/java/com/pracatice/Controller/QuizController.java
@@ -1,14 +1,19 @@
 package com.pracatice.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pracatice.Service.QuizService;
+import com.pracatice.entity.Question;
+import com.pracatice.entity.QuestionWrapper;
 
 @RestController
 @RequestMapping("quiz")
@@ -23,4 +28,8 @@ public class QuizController {
 		return quizService.createQuiz(category,numQ,title);
 	}
 	
+	@GetMapping("getQuiz/{idx}")
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(@PathVariable ("idx") Integer id){
+		return quizService.getQuizQuestion(id);
+	}
 }
--- QuizApp4/src/main/java/com/pracatice/Service/QuizService.java
+++ QuizApp4/src/main/java/com/pracatice/Service/QuizService.java
@@ -1,6 +1,8 @@
 package com.pracatice.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import com.pracatice.DAO.QuestionDao;
 import com.pracatice.DAO.QuizDao;
 import com.pracatice.entity.Question;
+import com.pracatice.entity.QuestionWrapper;
 import com.pracatice.entity.Quiz;
 
 @Service
@@ -36,6 +39,21 @@ public class QuizService {
 		
 		
 		return new ResponseEntity<>("Success ",HttpStatus.CREATED);
+	}
+	public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(Integer id) {
+		// TODO Auto-generated method stub
+		
+		Optional<Quiz> quiz = quizDao.findById(id);
+		List<Question> quesFromDb = quiz.get().getQuestion();
+		List<QuestionWrapper> questionForUser = new ArrayList<>();
+		
+		for(Question q : quesFromDb) {
+			QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getQuestionTitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+			questionForUser.add(qw);
+		}
+			
+		
+		return new ResponseEntity<>(questionForUser,HttpStatus.OK);
 	}
 	
 	
