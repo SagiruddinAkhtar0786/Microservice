76ff29afee28bbf3e3d2645acdd35c4be23bd57e
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-04-04 00:05:42 +0530

    commit

---
+++ QuizApp4-Microservice/src/main/java/com/pracatice/MicroService/QuizApp4MicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pracatice.MicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApp4MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizApp4MicroserviceApplication.class, args);
+	}
+
+}
---
+++ QuizApp4-Microservice/src/test/java/com/pracatice/MicroService/QuizApp4MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pracatice.MicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApp4MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
