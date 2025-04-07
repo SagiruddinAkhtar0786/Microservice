93be0577ce45d2e0e4a8c8d20e53eff9b99c2ebc
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2025-04-07 20:03:00 +0530

    commit

---
+++ API-gatewayMicroserviceProject/src/test/java/com/pracatice/api/gateway/ApiGatewayMicroserviceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pracatice.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayMicroserviceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
